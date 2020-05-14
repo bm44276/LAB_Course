b31d1e483c8bc7515ab10fdcdc925a7fbca2551a
Besmir <bm44276@ubt-uni.net>
2020-05-14 20:12:36 +0200

    sign up to check if fields are empty password needs to be grader than 6 characters  and display error message

--- SignUp.php
+++ SignUp.php
@@ -1,8 +1,8 @@
 <?php
 include_once "connectDB.php";
 
-session_start();
-header('location:index.html');
+
+    if(isset($_POST['submit'])){
 
 $FirstName = mysqli_real_escape_string($DB,$_POST["first-name"]);
 $LastName = mysqli_real_escape_string($DB,$_POST["last-name"]);
@@ -12,15 +12,27 @@ $Email = mysqli_real_escape_string($DB,$_POST["email"]);
 $Username = mysqli_real_escape_string($DB,$_POST["user-name"]);
 $Password = mysqli_real_escape_string($DB,$_POST["password"]);
 
-
+        if(empty($FirstName) || empty($FirstName) || empty($LastName) || empty($Age) || empty($Email) || empty($Username) || empty($Password)){
+            header("Location: index.php?signup=empty");
+        }else{
+            
 $usernametaken = "SELECT * FROM accounts WHERE Username = '$Username'";
 $result = $DB->query($usernametaken);
 $num = mysqli_num_rows($result);
 if($num == 1){
-    //username taken
+    header("Location: index.php?signup=usernameIsTaken");
 }else{
+
+    if(strlen("$Password") < 6){
+        header("Location: index.php?signup=passwordIsNotLongEnough");
+    }else{
     $myquery = "INSERT INTO accounts (Name,Surname,Age,Email,Username,Password) VALUES ('$FirstName','$LastName','$Age','$Email','$Username','$Password');";
     $DB->query($myquery);
-    header("location:thankYouPage.html");
+    header("Location:thankYouPage.html");
 }
+}
+  }
+    }else{
+        header("Location: index.php?signup=error");
+    }
 ?>
