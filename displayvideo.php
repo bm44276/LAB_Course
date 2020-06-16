027cf7313112367be3c2971c9c7d3c863d8fec8c
Jon Morina 23 <64468973+JonMorina@users.noreply.github.com>
2020-06-16 19:02:32 +0200

    Update displayvideo.php

--- displayvideo.php
+++ displayvideo.php
@@ -25,9 +25,20 @@
             video.controls = true;
             videodiv.style = "border:10px solid black;"
             videodiv.style = "text-align: center;";
-            videodiv.style.margin = "50px 0";
+           videodiv.style.margin = "30px 0";
+
+            let h1 = document.createElement("h1");
+                h1.innerHTML = localStorage.getItem("storageName1");
+                h1.style.textAlign = "Center";
+                h1.style.padding = "5px"
+                h1.style.fontSize = "30px";
+                h1.style.margin = "10px 0 30px 0";
+                h1.style.fontWeight = "900";
+                h1.style.letterSpacing = "5px";
+                document.body.appendChild(h1);
             videodiv.appendChild(video);
             document.body.appendChild(videodiv);
+
 
 
          </script>
